23ffc6cd79e4f126e33ceabf8b44a47cd5cfd86e
kyumho kim <rlaruagh030@gmail.com>
2023-12-14 12:56:00 +0900

    work!!

--- src/main/java/com/ll/medium/common/controller/HomeController.java
+++ src/main/java/com/ll/medium/common/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.ll.medium.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String home() {
         return "hello";
     }

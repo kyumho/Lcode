38c142bdb2db7db755461c6f3ed20d6c7555ee55
kyumho kim <rlaruagh030@gmail.com>
2023-12-14 11:09:51 +0900

    homecontroller 추가

---
+++ src/main/java/com/ll/medium/common/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ll.medium.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
+}
